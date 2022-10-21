5fde402b47eee5a5fec98860b4a4d32e9d7f383b
Jdavid Sepulveda V <jdavid.0794.sv@gmail.com>
2022-10-20 23:45:02 -0500

    update2.01

---
+++ CLASE2/conceptosB.js
@@ -0,0 +1,12 @@
+var nombre = 'alfonso';
+console.log(nombre);
+
+console.log(nombre.toUpperCase());  /* imprime la variable en MAYUSCULA */
+console.log(nombre.toLowerCase());  /* imprime la variable en MINUSCULA */
+console.log(nombre.startsWith("A"));  /* imprime TRUE - dado que la variable empieza por A */
+console.log(nombre.includes("t"));  /* imprime FALSE - dado que la variable NO contiene la T */
+console.log(nombre.endsWith("O"));  /* imprime true - dado que la variable termina en A */
+console.log("   Alfonso  ".trim()); /* Elimina los carácteres vacios de la variable */
+console.log(nombre.repeat(5)); /* repite la variable 5 veces */
+
+
